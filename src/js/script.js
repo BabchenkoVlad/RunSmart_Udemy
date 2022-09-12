190784d3ccb2e84e7182ee8255b0c070b13d79e5
BabchenkoVlad <babch.vl@gmail.com>
2022-09-12 15:04:29 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//   speed: 1000,
+//   // slidesToShow: 1,
+//   // centerMode: true,
+//   // variableWidth: true,
+//   // autoplay: true,
+//   // autoplaySpeed: 3000,
+//   // fade: true,
+//   // cssEase: 'linear',
+//   // adaptiveHeight: true,
+//   prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg></button>',
+//   nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg></button>',
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         dots: true
+//       }
+//     },
+//     // {
+//     //   breakpoint: 600,
+//     //   settings: {
+//     //     slidesToShow: 2,
+//     //     slidesToScroll: 2
+//     //   }
+//     // },
+//     // {
+//     //   breakpoint: 480,
+//     //   settings: {
+//     //     slidesToShow: 1,
+//     //     slidesToScroll: 1
+//     //   }
+//     // }
+//   ]
+// });
+// });
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     // controlText: [
+//     //   '<img src="../icons/left.svg>',
+//     //   '<img src="../icons/right.svg>'
+//     // ]
+//     controls: false,
+//     nav: false,
+//     responsive: {
+//       640: {
+//         edgePadding: 20,
+//         gutter: 20,
+//         items: 2
+//       },
+//       700: {
+//         gutter: 30
+//       },
+//       900: {
+//         items: 3
+//       }
+//     }
+// });
+  
+// document.querySelector('.prev').addEventListener('click', function () {
+//   slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//   slider.goTo('next');
+// });
