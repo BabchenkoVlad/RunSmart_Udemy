8944afac994d562fc305c03dbdaff59a2cef1964
BabchenkoVlad <babch.vl@gmail.com>
2022-09-15 15:35:26 +0300

    realize overlay

--- src/js/script.js
+++ src/js/script.js
@@ -72,7 +72,7 @@
 // });
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     // $('.carousel__inner').slick({
     //     speed: 1200,
     //     adaptiveHeight: true,
@@ -89,15 +89,15 @@ $(document).ready(function(){
     //     ]
     // });
     
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -107,4 +107,23 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order, .modal_descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    $('.feed-form').validate();
+    $('#consultation').validate();
+    $('#order').validate();
 });
