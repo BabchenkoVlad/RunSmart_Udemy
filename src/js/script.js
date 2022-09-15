4c4e5a8332b18646479780e193410ec027593b80
BabchenkoVlad <babch.vl@gmail.com>
2022-09-15 19:40:13 +0300

    home work modal bootstrap

--- src/js/script.js
+++ src/js/script.js
@@ -108,22 +108,45 @@ $(document).ready(function () {
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    // modal
-    $('[data-modal=consultation]').on('click', function () {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    });
+    // // modal
+    // $('[data-modal=consultation]').on('click', function() {
+    //     $('.overlay, #consultation').fadeIn('slow');
+    // });
+    // $('.modal__close').on('click', function() {
+    //     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    // });
 
-    $('.button_mini').each(function (i) {
-        $(this).on('click', function () {
-            $('#order, .modal_descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        });
-    });
+    // $('.button_mini').each(function(i) {
+    //     $(this).on('click', function() {
+    //         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    //         $('.overlay, #order').fadeIn('slow');
+    //     })
+    // });
 
-    $('.feed-form').validate();
-    $('#consultation').validate();
-    $('#order').validate();
+    // function validateForms(form) {
+    //     $(form).validate({
+    //         rules: {
+    //             name: "required",
+    //             phone: "required",
+    //             email: {
+    //                 required: true,
+    //                 email: true
+    //             }
+    //         },
+    //         messages: {
+    //             name: "Пожалуйста введите свое имя",
+    //             phone: "Пожалуйста введите свой телефон",
+    //             email: {
+    //               required: "Пожалуйста введите свою почту",
+    //               email: "Неправильно введен адрес почты"
+    //             }
+    //         }
+    //     })
+    // };
+
+    // $('.consultation-form').validate();
+    // $('#consultation form').validate();
+    // $('#order form').validate();
+
+    // $('input[name=phone]').mask("+38 (099) 999-99-99")
 });
